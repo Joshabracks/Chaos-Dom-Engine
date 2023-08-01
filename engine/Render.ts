c9c390540fd71276517691f02e94759c6ae02a25
Joshua Bracks <joshabracks@gmail.com>
2023-08-01 00:15:02 -0700

    fix(): objects render recursively

--- engine/Render.ts
+++ engine/Render.ts
@@ -1,6 +1,13 @@
 import Application from './Application'
+import { GameObject } from './GameObject'
 import { pollEvents } from './Input'
 import { Scene, getActiveScene } from './SceneManager'
+
+function renderObjectRecursive(object: GameObject, scene: Scene, delta: number) {
+  if (object.update && object.active) object.update(object, delta)
+  scene.camera.render(object)
+  object.children.forEach((child: GameObject) => renderObjectRecursive(child, scene, delta))
+}
 
 function renderLoop(timestamp = 0) {
   if (!Application.RUNNING) window.close()
@@ -8,10 +15,7 @@ function renderLoop(timestamp = 0) {
   const delta = timestamp - Application.PREVIOUS_TIME_STAMP
   if (delta > Application.TARGET_MS) Application.PREVIOUS_TIME_STAMP = timestamp
   const scene: Scene = getActiveScene() as Scene
-  scene.objects.forEach(object => {
-    if (object.update && object.active) object.update(object, delta)
-    scene.camera.render(object)
-  })
+  scene.objects.forEach((object: GameObject) => renderObjectRecursive(object, scene, delta))
   pollEvents()
   requestAnimationFrame(renderLoop)
 }
