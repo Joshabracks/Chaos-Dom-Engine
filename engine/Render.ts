14e59eb1cb88653073129ff5bc186744c85ef161
Joshua Bracks <joshabracks@gmail.com>
2023-08-05 00:08:38 -0700

    fix(): remove window scroll

--- engine/Render.ts
+++ engine/Render.ts
@@ -16,9 +16,6 @@ function renderLoop(timestamp = 0) {
   const delta = timestamp - Application.PREVIOUS_TIME_STAMP
   if (delta > Application.TARGET_MS) Application.PREVIOUS_TIME_STAMP = timestamp
   const scene: Scene = getActiveScene() as Scene
-  if (scene.camera.position.x < 0) scene.camera.position.x = 0
-  if (scene.camera.position.y < 0) scene.camera.position.y = 0
-  window.scroll(scene.camera.position.x * Application.SCALE, scene.camera.position.y * Application.SCALE)
   scene.objects.forEach((object: GameObject) => renderObjectRecursive(object, scene, delta))
   requestAnimationFrame(renderLoop)
 }
